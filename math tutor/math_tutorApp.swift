c2b9a0fe9303508065976d2f347aa9d2fa9b61fa
IJuarez56 <ijuarzez1234@gmail.com>
2026-02-03 12:08:05 -0800

    Initial Commit

---
+++ math tutor/math_tutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  math_tutorApp.swift
+//  math tutor
+//
+//  Created by JUAREZ, ISABELLA on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct math_tutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
